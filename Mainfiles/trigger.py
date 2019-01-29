250a3ceca78f41b7ffb15c92038c9e1a884d74f5
Kwazinator <46730201+Kwazinator@users.noreply.github.com>
2019-01-29 13:19:18 -0500

    Update trigger.py
    added channels messages and sender in string to trigger.py

--- Mainfiles/trigger.py
+++ Mainfiles/trigger.py
@@ -3,7 +3,7 @@ import datetime
 import subprocess
 ######################################START DEF METHODS##############################################
 # for transparency, variables saved to files since program might restart often
-def igiveup():
+def igiveup(channel, message, sender):
     with open('dayandhasconfig.txt', 'r') as file:
         data = file.read()
     if data < str(datetime.datetime.now().day) and datetime.datetime.now().hour >= 15:
@@ -17,35 +17,38 @@ def igiveup():
             answer = myfile.read()
     return answer
 
-def whoami():
+def whoami(channel, message, sender):
     return "You are you."
 
-def whoamireally():
+def whoamireally(channel, message, sender):
     return "I am he, as you are he, as you are me and we are all together."
 
-def shellcommands():
+def shellcommands(channel, message, sender):
     # try catch block with subprocess in it. XD
     return "comming soon"
 
+def test(channel, message, sender):
+    return channel + ' ' + message + ' ' + sender
 
-def help():
+def help(channel, message, sender):
     return "!igiveup: provides a solution to the daily Robot Reboot Challenge !help: provides this wonderful text !reminder: dont do nuffing yet"
 
 
-def reminder():
+def reminder(channel, message, sender):
     return "reminder.txt for specific people maybe set timers? and it will @them reminding them like an alarmclock"
 ######################################END DEF METHODS#################################################
 
 
 #add the "msgtotrigger": (function returning message to send), dont forget the comma!
-def GetTrigger():
+def GetTrigger(channel, message, sender):
     triggers = {
-        "!igiveup": igiveup(),
-        "!help": help(),
-        "!reminder": reminder(),
-        "!whoami": whoami(),
-        "!whoamireally": whoamireally(),
-        "!shellcommands": shellcommands()
+        "!igiveup": igiveup(channel, message, sender),
+        "!help": help(channel, message, sender),
+        "!reminder": reminder(channel, message, sender),
+        "!whoami": whoami(channel, message, sender),
+        "!whoamireally": whoamireally(channel, message, sender),
+        "!shellcommands": shellcommands(channel, message, sender),
+        "!test": test(channel, message, sender)
     }
     return triggers
 
