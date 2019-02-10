869e99f9b9e5e45c5d78f5f37c0691bd9dc1f97f
Kwazinator <kwasiky@gmail.com>
2019-02-10 11:46:29 -0500

    googleing images

--- Mainfiles/trigger.py
+++ Mainfiles/trigger.py
@@ -34,6 +34,13 @@ def test(channel, message, sender):
     return channel + ' ' + message + ' ' + sender
 
 def google(channel, message, sender):
+
+    response = google_images_download.googleimagesdownload()
+
+    arguments = {"keywords":"Polar bears","limit":20,"print_urls":True}
+    paths = response.download(arguments)
+    print(paths)
+
     return ""
 
 
