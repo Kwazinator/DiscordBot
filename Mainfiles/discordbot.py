8b54c41d456c8fac12e3c620e9a2ffc5e799e3e9
Kwazinator <46730201+Kwazinator@users.noreply.github.com>
2019-02-28 10:52:00 -0500

    Update discordbot.py

--- Mainfiles/discordbot.py
+++ Mainfiles/discordbot.py
@@ -83,7 +83,7 @@ class MyClient(discord.Client):
             elif checkmessage == '!root':
                 if (sender == 'chan2#2445'):
                     try:
-                        subprocess.call(utf8message[5:], shell=True)
+                        msg = subprocess.check_output([utf8message[5:]])
                     except Exception as e:
                         msg = e
                     finally:
