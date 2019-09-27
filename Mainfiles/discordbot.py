5b1faf53fb2f614ab1d498dbf0056d88909e3711
Kwazinator <46730201+Kwazinator@users.noreply.github.com>
2019-09-27 15:04:35 -0400

    Update discordbot.py

--- Mainfiles/discordbot.py
+++ Mainfiles/discordbot.py
@@ -71,8 +71,10 @@ class MyClient(discord.Client):
                 picture = filename + '.png'
                 await message.channel.send(file=discord.File(fp=picture, filename=(picture + '.png')))
             elif utf8message.endswith('???') or utf8message.endswith('!poll'):
-                await message.add_reaction('✖️')
-                await message.add_reaction('☑️')
+                await message.add_reaction('\N{THUMBS UP SIGN}')
+                await message.add_reaction('\N{THUMBS DOWN SIGN}')
+                await message.add_reaction('\N{CROSS MARK}')
+                await message.add_reaction('\N{CHECK MARK}')
             elif checkmessage == "!del":
                 if (sender == 'Svlad_Cjelli#0042' or sender =='chan2#2445'):
                     try:
