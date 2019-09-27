f529307f7ba56f7c42246cdb79500dbdae2b179b
Kwazinator <46730201+Kwazinator@users.noreply.github.com>
2019-09-27 15:28:49 -0400

    Update discordbot.py

--- Mainfiles/discordbot.py
+++ Mainfiles/discordbot.py
@@ -75,7 +75,7 @@ class MyClient(discord.Client):
                 await message.add_reaction('\N{THUMBS DOWN SIGN}')
                 await message.add_reaction('\N{CROSS MARK}')
                 await message.add_reaction('\N{NEGATIVE SQUARED CROSS MARK}')
-                await message.add_reaction('\N{WHITE CHECK MARK}')
+                await message.add_reaction('\N{BALLOT BOX WITH CHECK}')
             elif checkmessage == "!del":
                 if (sender == 'Svlad_Cjelli#0042' or sender =='chan2#2445'):
                     try:
