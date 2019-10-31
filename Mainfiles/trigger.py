0873c41495bfea7f7e04f849355cece32ad85fcc
Kwazinator <46730201+Kwazinator@users.noreply.github.com>
2019-10-31 16:43:57 -0400

    Update trigger.py

--- Mainfiles/trigger.py
+++ Mainfiles/trigger.py
@@ -72,7 +72,7 @@ def minecraft(channel, message, sender):
     elif message == 'status':
         client = get_aws_client_instance()
         response = client.describe_instances()
-        return str(response)
+        print(str(response))
     else:
         return 'Unknown command, use:\n!minecraft start\n!minecraft stop\n!minecraft status'
         
